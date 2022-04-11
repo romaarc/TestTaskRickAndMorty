70f9aa0e3858ce5687e137a5805619daf3a3020e
Roman <romangorshkovreme@gmail.com>
2022-04-11 22:40:59 +0300

    CharacterDetailViewCell func setupUI - fix selfs

--- TestTaskRickAndMorty/Source/Coordinator/AppCoordinator.swift
+++ TestTaskRickAndMorty/Source/Coordinator/AppCoordinator.swift
@@ -52,7 +52,6 @@ private extension AppCoordinator {
                                                         tag: navigationControllerKey.rawValue)
             navigationController.tabBarItem = tabBarItem
             navigationController.navigationBar.prefersLargeTitles = true
-            //navigationController.navigationItem.largeTitleDisplayMode = .always
             navigationController.navigationBar.sizeToFit()
             result[navigationControllerKey] = navigationController
         }
